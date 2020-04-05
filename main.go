94f6f3ee2b2cac428b818150ecc0b1ee8ffc81ae
Marcos Lemus <mlemus@codeputing.com>
2020-04-04 22:40:13 -0500

    delete hacky go routine

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"syscall/js"
 )
 
+const ITTERATION_LIMIT = 50
+
 var (
 	// js.Value can be any JS object/type/constructor
 	window, doc, body, canvas, ctx, img, pixels js.Value
@@ -37,21 +39,20 @@ func makeItPink(ctx js.Value) {
 	pixels = img.Get("data")
 
 	for x := 0; x < int(windowSize.w); x++ {
-		go strip(x)
-		// for y := 0; y < int(windowSize.h); y++ {
-		// 	index := (y*int(windowSize.w) + x) * 4
-		// 	cx := ((float64(x) - windowSize.w/2) / windowSize.w) * 4
-		// 	cy := ((float64(y) - windowSize.h/2) / windowSize.h) * 4
+		for y := 0; y < int(windowSize.h); y++ {
+			index := (y*int(windowSize.w) + x) * 4
+			cx := ((float64(x) - windowSize.w/2) / windowSize.w) * 4
+			cy := ((float64(y) - windowSize.h/2) / windowSize.h) * 4
 
-		// 	color := float64(mandelbrot(cx, cy, 2))
-		// 	l := float64(limit)
-		// 	green := 255 - ((l-color)/l)*255.
+			color := float64(mandelbrot(cx, cy, 2))
+			l := float64(ITTERATION_LIMIT)
+			green := 255 - ((l-color)/l)*255.
 
-		// 	pixels.SetIndex(index, 0)
-		// 	pixels.SetIndex(index+1, green)
-		// 	pixels.SetIndex(index+2, 0)
-		// 	pixels.SetIndex(index+3, 255)
-		// }
+			pixels.SetIndex(index, 0)
+			pixels.SetIndex(index+1, green)
+			pixels.SetIndex(index+2, 0)
+			pixels.SetIndex(index+3, 255)
+		}
 	}
 
 	img.Set("data", pixels)
@@ -59,32 +60,10 @@ func makeItPink(ctx js.Value) {
 	fmt.Println("It's PINK")
 }
 
-func strip(x int) {
-	for y := 0; y < int(windowSize.h); y++ {
-		index := (y*int(windowSize.w) + x) * 4
-		cx := ((float64(x) - windowSize.w/2) / windowSize.w) * 4
-		cy := ((float64(y) - windowSize.h/2) / windowSize.h) * 4
-
-		color := float64(mandelbrot(cx, cy, 2))
-		l := float64(limit)
-		green := 255 - ((l-color)/l)*255.
-
-		pixels.SetIndex(index, 0)
-		pixels.SetIndex(index+1, green)
-		pixels.SetIndex(index+2, 0)
-		pixels.SetIndex(index+3, 255)
-	}
-
-	img.Set("data", pixels)
-	ctx.Call("putImageData", img, 0, 0)
-}
-
-const limit = 50
-
 func mandelbrot(x, y, threshold float64) int {
 	c0 := complex(x, y)
 	c := complex(0, 0)
-	for i := 0; i < limit; i++ {
+	for i := 0; i < ITTERATION_LIMIT; i++ {
 		if cmplx.Abs(c) > threshold {
 			return i
 		}
